5760acef9c1c248358926ff541b0494ac29dc5f4
prkksh <jpdmprksh@gmail.com>
2019-05-14 15:01:57 +0000

    Add Serialize func

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,8 @@
+import os
+
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,24 +16,36 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
-    id = Column(
-    Integer, primary_key = True)
-    name = Column(
-    String(250), nullable = False)
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+
+    @property
+    def serialize(self):
+        return{
+            'name': self.name,
+            'id': self.id
+        }
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    id = Column(
-    Integer, primary_key = True)
-    name = Column(
-    String(100), nullable = False)
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = False)
     course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
     restaurant_id = Column(
     Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        return{
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'price': self.price
+        }
 
 
 engine = create_engine('sqlite:///catalog.db')
