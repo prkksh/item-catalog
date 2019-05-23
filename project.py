1e66247f3b78fb316c8bdf4c985b96be4a0dcac6
prkksh <jpdmprksh@gmail.com>
2019-05-23 14:02:29 +0000

    Update Login func

--- project.py
+++ project.py
@@ -111,12 +111,6 @@ def gconnect():
     # ADD PROVIDER TO LOGIN SESSION
     login_session['provider'] = 'google'
 
-    # see if user exists, if it doesn't make a new one
-    user_id = getUserID(login_session['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-    login_session['user_id'] = user_id
-
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
